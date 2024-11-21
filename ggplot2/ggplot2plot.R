56d0b10bc81bdf94d4a1a2ec134b7898df963aee
Kwan Yu Chuk <24345562@student.uwa.edu.au>
2024-11-21 22:36:43 +0800

    Project added

---
+++ ggplot2/ggplot2plot.R
@@ -0,0 +1,24 @@
+# author: Kwan Yu Chuk
+
+library(ggplot2)
+
+# example 1
+# this code is to create a bar chart with legend and annotate
+# data were grouped by site and colours were identical
+ground <- read.csv("ground.csv", stringsAsFactors = T)
+head(ground)
+
+ground_p <- ggplot(ground, aes(x=site, y=count, fill=survey)) + 
+  geom_bar(position="dodge", stat="identity", width = 0.7) + 
+  labs(y="Number of pigeons per camera per site") + 
+  xlab(NULL)+
+  
+  theme_bw() + 
+  theme(legend.position=c(0.05, 0.95),
+        legend.justification = c(0, 1),
+        legend.text = element_text(size = 20),
+        legend.title = element_blank(),
+        axis.text.x = element_text(size = 25, hjust = 0.5),
+        axis.text.y = element_text(size = 20, hjust = 0.5),
+        axis.title.y = element_text(size = 25))
+ground_p + annotate('text', x = 2, y = 1500, label = 'Ground', family = 'serif', fontface = 'italic', size = 15)
